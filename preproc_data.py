a5ad777dad0e26e91c7cb89860e8b953289879a4
sejo95 <johann.seltmann@gmx.net>
2019-07-05 18:23:31 +0000

    bugfixes in preproc_data.py

--- preproc_data.py
+++ preproc_data.py
@@ -13,7 +13,7 @@ import random
 config_path = os.environ.get('VISCONF')
 if not config_path:
     # try default location, if not in environment
-    default_path_to_config = '../../clp-vision/Config/gpu_potsdam.cfg'
+    default_path_to_config = '../../SwP/clp-vision/Config/gpu_potsdam.cfg'
     if os.path.isfile(default_path_to_config):
         config_path = default_path_to_config
 
@@ -35,7 +35,7 @@ from wac_utils import create_word2den, is_relational
 sys.path.append(dsgv_home + '/Preproc')
 from sim_preproc import load_imsim, n_most_sim
 
-sys.path.append('../Common')
+sys.path.append('../../SwP/sempix/Common')
 from data_utils import load_dfs, plot_rel_by_relid, get_obj_bb, compute_distance_objs
 from data_utils import get_obj_key, compute_relpos_relargs_row, get_all_predicate
 from data_utils import compute_distance_relargs_row, get_rel_type, get_rel_instances
@@ -76,7 +76,7 @@ df['vgpardf']['coco_image_id'] = df['vgpardf']['image_id'].apply(lambda x: vg2co
 df['cocoparcapdf'] = df['cococapdf'].merge(df['vgpardf'],
                                            left_on='image_id', right_on='coco_image_id')
 
-def create_binary_dataset(data_filename, sim_rank=0, train_split=[0.9,0.1,0.1]):
+def create_binary_dataset(data_filename, sim_rank=0, train_split=[0.8,0.1,0.1]):
     """
     Create a datafile for the caption-to-there-is 
     entailment as binary classification.
@@ -92,13 +92,27 @@ def create_binary_dataset(data_filename, sim_rank=0, train_split=[0.9,0.1,0.1]):
     train_split : [float]
         Fractions of data to use for train, dev, and test set.
     """
-    #pos_dict = dict()
     data_points = []
-    #neg_dict = dict()
-    ic = icorpus_code['mscoco']
-    
+
+    phyps = set()
+    nhyps = set()
+    vgiis = set()
+    i = 0
+
     for _, row in visgencocap_regdf.iterrows():
         vgii = row['image_id']
+        if vgii in vgiis:
+            continue
+
+        if i == 10:
+            print("10")
+        if i == 100:
+            print("100")
+        if i % 5000 == 0:
+            print("processed", i, "rows ...")
+        vgiis.add(vgii)
+        i += 1
+
         cocoii = row['coco_id']
 
         # get objects in image
@@ -107,57 +121,60 @@ def create_binary_dataset(data_filename, sim_rank=0, train_split=[0.9,0.1,0.1]):
             #if an object has no syn_id ignore this caption
             continue
 
-        phyps = set(df['vgobjdf'][df['vgobjdf']['image_id'] == vgii]['name'].tolist())
-        #pos_dict[vgii] = phyps
-        data_points += [(vgii, phyp, True) for phyp in phyps]
+        curr_phyps = set(df['vgobjdf'][df['vgobjdf']['image_id'] == vgii]['name'].tolist())
+        curr_phyps = [(vgii, phyp, True) for phyp in curr_phyps]
+        phyps.update(curr_phyps)
 
+        try:
+            ignore_sem_ids = n_most_sim(visg_sem_sim, visg_sem_map,
+                                        visg_id2semsim[vgii], n=sim_rank)
+            ignore_vis_ids = n_most_sim(visg_vis_sim, visg_vis_map,
+	                                visg_id2vissim[vgii], n=sim_rank)
+            ignore_ids = ignore_vis_ids + ignore_sem_ids
+            ignore_ids.append(vgii)
+        except Exception as e:
+            ignore_ids = []
 
-        ignore_sem_ids = n_most_sim(visg_sem_sim, visg_sem_map,
-                                    visg_id2semsim[vgii], n=sim_rank)
-        ignore_ids = list(map(lambda x: visg_sem_map, ignore_sem_ids))
-        ignore_vis_ids = n_most_sim(visg_vis_sim, visg_vis_map,
-                                    visg_id2vissim[vgii], n=sim_rank)
-        ignore_ids += list(map(lambda x: visg_vis_map, ignore_vis_ids))
-        ignore_ids.append(vgii)
-
-        
-        neg_num = len(phyps)
-        nhyps = set()
-        while len(nhyps) < neg_num:
+        neg_num = len(curr_phyps)
+        curr_nhyps = set()
+        while len(curr_nhyps) < neg_num:
             neg_sample = df['vgobjdf'].sample()
-            negii = neg_sample['image_id']
+            negii = neg_sample['image_id'].values[0]
             if negii in ignore_ids:
                 continue
-            neg_obj = neg_sample['syn']
+            neg_obj = neg_sample['syn'].values[0]
             if neg_obj in obj_syns:
                 continue
-            neg_name = neg_sample['name']
-            nhyps.add((vgii, neg_name, False))
+            neg_name = neg_sample['name'].values[0]
+            curr_nhyps.add((vgii, neg_name, False))
+        nhyps.update(curr_nhyps)
+
+    print("found hypotheses")
+
+    phyps = list(phyps)
+    nhyps = list(nhyps)
+    np.random.shuffle(phyps)
+    np.random.shuffle(nhyps)
+
+    plen = len(phyps)
+    train_len = int(train_split[0] * plen)
+    dev_len = int(train_split[1] * plen)
+
+    train_hyps = phyps[:train_len] + nhyps[:train_len]
+    np.random.shuffle(train_hyps)
+    dev_hyps = phyps[train_len:(train_len+dev_len)] + nhyps[train_len:(train_len+dev_len)]
+    np.random.shuffle(dev_hyps)
+    test_hyps = phyps[train_len+dev_len:] + nhyps[train_len+dev_len:]
+    np.random.shuffle(test_hyps)
+
+    data = dict()
+    data['train_hyps'] = train_hyps
+    data['dev_hyps'] = dev_hyps
+    data['test_hyps'] = test_hyps
+
+    with open(data_filename, "wb") as data_file:
+        pickle.dump(data, data_file)
 
 
-        #total_len = len(all_hyps)
-        #train_len = int(train_split[0] * total_len)
-        #dev_len = int(train_split[1] * total_len)
-        plen = len(phyps)
-        train_len = int(train_split[0] * plen)
-        dev_len = int(train_split[1] * plen)
 
-        train_hyps = phyps[:train_len] + nhyps[:train_len]
-        dev_hyps = phyps[train_len:dev_len] + nhyps[train_len:dev_len]
-        test_hyps = phyps[train_len+dev_len:] + nhyps[train_len+dev_len:]
-
-        data = dict()
-        data['train_hyps'] = train_hyps
-        data['dev_hyps'] = dev_hyps
-        data['test_hyps'] = test_hyps
-
-        with open(data_filename, "wb") as data_file:
-            pickle.dump(data, data_file)
-
-
-        
-            
-        
-
-
-
+create_binary_dataset("../data/binary_class.pkl", sim_rank=10, train_split=[0.8,0.1,0.1])
