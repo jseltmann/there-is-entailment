da23499e8554a842160835d200ceefb9b77cecc6
Vincent Ortland <vincent.ortland@gmx.de>
2019-09-09 16:04:22 +0000

    added wordnet LSTM

---
+++ class_true_false/wordnet/basic_model.py
@@ -0,0 +1,53 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class BaseLSTM(nn.Module):
+
+    def __init__(self, input_size=1, hidden_size=100, batch_size=64):
+        super(BaseLSTM, self).__init__()
+        self.hidden_size = hidden_size
+        self.batch_size = batch_size
+        
+        self.caption_lstm = nn.LSTM(input_size,
+                                    self.hidden_size,
+                                    bidirectional=True)
+        self.object_lstm = nn.LSTM(input_size,
+                                   self.hidden_size,
+                                   bidirectional=True)
+
+        self.linear1 = nn.Linear(4 * self.hidden_size + 1, 4 * self.hidden_size + 1)
+        self.linear2 = nn.Linear(4 * self.hidden_size + 1, 2)
+
+    def forward(self, caption, obj, dist):
+        _, (h_cap, _) = self.caption_lstm(caption)
+        _, (h_obj, _) = self.object_lstm(obj)
+
+        # h_cap is tuple, since bidirectional lstm
+        h_cap = torch.stack((h_cap[0], h_cap[1]), dim=1)
+        h_cap = h_cap.reshape(-1, 2 * self.hidden_size)
+        h_obj = torch.stack((h_obj[0], h_obj[1]), dim=1)
+        h_obj = h_obj.reshape(-1, 2 * self.hidden_size) # (BATCH_SIZE * HIDDEN_SIZE)
+
+        dist = dist.reshape(-1, 1)
+        h_combined = torch.cat([h_cap, h_obj, dist], 1)
+
+        l1 = self.linear1(h_combined)
+        out = self.linear2(l1)
+
+        return out
+
+    def reset_state(self):
+        """
+        Reset hidden states to zero.
+        """
+        self.hidden = (torch.zeros(1, self.batch_size, self.hidden_size),
+                       torch.zeros(1, self.batch_size, self.hidden_size))
+
+
+
+         
+        
+
+
---
+++ class_true_false/wordnet/train_basic_model.py
@@ -0,0 +1,157 @@
+import torch
+import torch.nn.utils.rnn as rnn
+import nltk
+import csv
+import pickle
+
+from nltk.corpus import wordnet
+from basic_model import BaseLSTM
+
+nltk.download('punkt')
+nltk.download('wordnet')
+
+MAX_LEN = 25
+BATCH_SIZE = 64
+NUM_EPOCHS = 3
+LEARNING_RATE = 1e-4
+
+INPUT_SIZE = 1
+HIDDEN_SIZE = 25#100
+with open("../../data/bert_classify_thereis_5caps_seed0/word_inds.pkl", "rb") as word_ind_file:
+    word2num, _ = pickle.load(word_ind_file)
+    PAD_INDEX = word2num["<PAD>"]
+
+def load_data(train_filename, word_ind_filename, batch_size=64):
+    """
+    Load training data and translate the words to indices.
+
+    Parameters
+    ----------
+    train_filename : str
+        Filename of a csv file containg the training data.
+    word_ind_filename : str
+        Filename of a file containing dicts that translate
+        from words to indices and from indices to words, respectively.
+    batch_size : int
+        batch size
+    """
+
+    inputs = []
+    caps = []
+    objs = []
+    dists = []
+    labels = []
+
+    with open(word_ind_filename, "rb") as word_ind_file:
+        word2ind, ind2word = pickle.load(word_ind_file)
+
+    with open(train_filename) as train_file:
+        train_reader = csv.reader(train_file, delimiter='\t', quotechar=None, escapechar="\\")
+        for i, line in enumerate(train_reader):
+            if i == 0:
+                continue
+            caption = line[1].lower()
+            obj = line[2].lower()
+            label = line[3]
+            
+            cap_words = nltk.word_tokenize(caption)
+            cap_words = cap_words[:MAX_LEN]
+            cap_inds = torch.tensor([float(word2ind[word]) for word in cap_words])
+            caps.append(cap_inds)
+
+            obj_words = nltk.word_tokenize(obj)
+            obj_words = obj_words[:MAX_LEN] # unlikely
+            obj_inds = torch.tensor([float(word2ind[word]) for word in obj_words])
+            objs.append(obj_inds)
+
+            # calculate the path similarity of the words in the caption to the object word
+            o = wordnet.synsets(nltk.word_tokenize(obj)[0])
+            dist_vals = [.0 for w in nltk.word_tokenize(caption)]
+            if len(o) > 0:
+                o = o[0]
+                dists_val = max([wordnet.synsets(w)[0].path_similarity(o) if len(wordnet.synsets(w))>0 and wordnet.synsets(w)[0].path_similarity(o) != None else 0. for w in nltk.word_tokenize(caption)])
+            dists_val = torch.tensor(dists_val)
+            dists.append(dists_val) 
+            #inputs.append((cap_inds, obj_inds))
+            
+            if label == "True":
+                labels.append(torch.tensor([0.0,1.0]))
+            elif label == "False":
+                labels.append(torch.tensor([1.0,0.0]))
+            else:
+                raise Exception("label neither true nor false")
+
+            if i % 50000 == 0:
+                print("read", i, "training examples") 
+
+    caps_batched = []
+    while caps != []:
+        caps_batched.append(caps[:batch_size])
+        caps = caps[batch_size:]
+
+    objs_batched = []
+    while objs != []:
+        objs_batched.append(objs[:batch_size])
+        objs = objs[batch_size:]
+
+    labels_batched = []
+    while labels != []:
+        labels_batched.append(labels[:batch_size])
+        labels = labels[batch_size:]
+
+    dists_batched = []
+    while dists != []:
+        dists_batched.append(dists[:batch_size])
+        dists = dists[batch_size:]
+    #return inputs_batched, labels_batched
+    return caps_batched, objs_batched, labels_batched, dists_batched
+
+
+caps, objs, labels, dists = load_data("../../data/bert_classify_thereis_5caps_seed0/train.tsv",
+                           "../../data/bert_classify_thereis_5caps_seed0/word_inds.pkl",
+                           batch_size=BATCH_SIZE)
+with open("../../data/bert_classify_thereis_5caps_seed0/lstm_preprocessed_train.pkl", "wb") as processed_file:
+    pickle.dump((caps, objs, labels, dists), processed_file)
+with open("../../data/bert_classify_thereis_5caps_seed0/lstm_preprocessed_train.pkl", "rb") as processed_file:
+    caps, objs, labels, dists = pickle.load(processed_file)
+
+num_batches = len(caps)
+
+print("loaded data")
+
+model = BaseLSTM(INPUT_SIZE, HIDDEN_SIZE, BATCH_SIZE)
+
+loss_fn = torch.nn.MSELoss(size_average=False)
+optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+for epoch in range(NUM_EPOCHS):
+
+    i = 0
+    for (cap_batch, obj_batch, dists_batch), label_batch in list(zip(zip(caps, objs, dists),labels)):
+        cap_batch.sort(key=len, reverse=True)
+        cap_batch = rnn.pack_sequence(cap_batch)#, padding_value=PAD_INDEX)
+        cap_batch, _ = rnn.pad_packed_sequence(cap_batch, padding_value=PAD_INDEX)
+        cap_batch = cap_batch.unsqueeze(2)
+
+        obj_batch.sort(key=len, reverse=True)
+        obj_batch = rnn.pack_sequence(obj_batch)
+        obj_batch, _ = rnn.pad_packed_sequence(obj_batch, padding_value=PAD_INDEX)
+        obj_batch = obj_batch.unsqueeze(2)
+
+        dists_batch = torch.stack(dists_batch)
+        label_batch = torch.stack(label_batch)
+
+        model.zero_grad()
+        model.reset_state()
+
+        preds = model(cap_batch, obj_batch, dists_batch)
+        loss = loss_fn(preds, label_batch)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        if i % 5000 == 0:
+            print("epoch:", epoch, "batch:", i, "out of", num_batches)
+        i += 1
+
+torch.save(model.state_dict(), "../../logs/base_lstm_classification/models/base_2019-08-02-2lin.pt")
